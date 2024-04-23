955d2d4754fc0b654a9e8432f49b7defa7425fae
komiya7249 <komiyasakura@komiyasakura-no-MacBook-Pro.local>
2024-04-23 22:01:36 +0900

    weatherコントローラーの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_14_104804) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_23_125841) do
+  create_table "weathers", force: :cascade do |t|
+    t.integer "HighestTemperature"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
