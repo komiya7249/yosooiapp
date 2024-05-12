a6c8122d18b6c777ea2b8487c2dcbbe6e93fddc8
komiya7249 <komiyasakura@komiyasakura-no-MacBook-Pro.local>
2024-05-12 15:36:04 +0900

    「過去の天気と比較部分」のレイアウト追加・topページのレイアウト修正

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,8 +1,8 @@
 module HomeHelper
   def self.weather_code_return(code)
-    if code === 0
+    if code === 0 || code === 2
       return "sun"
-    elsif code >= 1 && code <= 3
+    elsif code === 3
       return "cloud"
     elsif code >= 45 && code <= 57
       return "fog"
--- app/models/weather_map.rb
+++ app/models/weather_map.rb
@@ -1,3 +1,2 @@
 class WeatherMap < ApplicationRecord
-
 end
